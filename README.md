f3298094b33dfd17e32856ea757bc25363ccbc5d
Daniel Carvalho <danielcarvalhodev@gmail.com>
2025-11-22 16:58:33 -0300

    Simplified ansible structure

--- README.md
+++ README.md
@@ -11,16 +11,4 @@ My personal ansible script to configure my personal server.
 3. Run the command bellow to run the ansible script. 
 **DON'T FORGET CHANGE SERVER IP INTO hosts.yml**
 
-`ansible-playbook playbooks/main.yml -i inventory/hosts.yml --user=root --ask-pass`
-
-
-
-# Script to install syncthing
-**Seems running the script below with ansible script not working correctly, please run manually the commands bellow!**
-
-```
-systemctl start syncthing@syncthing.service --now
-sed -i 's/127\.0\.0\.1:8384/SERVER_IP:8384/' /home/syncthing/.local/state/syncthing/config.xml
-systemctl restart syncthing@syncthing.service 
-systemctl enable syncthing@syncthing.service 
-```
+`ansible-playbook playbooks/main.yml -i inventory/hosts.yml --user=root`
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+apt remove -y $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
+
+# Add Docker's official GPG key:
 apt update
-apt install ca-certificates curl
+apt install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
 apt update
 
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
